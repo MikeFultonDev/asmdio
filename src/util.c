af32b9a1594e46164e99d53cbfda9db6209f8551
Anthony Giorgio <agiorgio@us.ibm.com>
2024-08-27 10:44:41 -0400

    New util.c module

---
+++ src/util.c
@@ -0,0 +1,17 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+
+int uppercase(char *string) {
+  if (!string) {
+    return -1;
+  }
+
+  const size_t len = strlen(string);
+
+  for (int i = 0; i < len; i++) {
+    string[i] = toupper(string[i]);
+  }
+
+  return 0;
+}
