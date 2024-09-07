24480b2d4e10f53dea79662153fb817475de0c19
Mike Fulton <mikefultonpersonal@gmail.com>
2024-09-07 00:30:42 -0400

    get the list of extensions
    Signed-off-by: Mike Fulton <mikefultonpersonal@gmail.com>

--- sample/f2m.c
+++ sample/f2m.c
@@ -3,6 +3,7 @@
 #include <glob.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 static void syntax(FILE* stream)
 {
@@ -29,6 +30,18 @@ typedef struct {
   int map:1;
 } F2M_Opts;
 
+typedef struct {
+  const char* key;
+  char** values;
+  int count;
+} F2M_Entry;
+
+typedef struct {
+  size_t max_size;
+  size_t size;
+  F2M_Entry* entry;
+} F2M_Table;
+
 static void init_opts(F2M_Opts* opts) {
   opts->help = 0;
   opts->map  = 0;
@@ -46,6 +59,75 @@ static int errfunc(const char *epath, int eerrno)
   return -1;
 }
 
+static F2M_Table* create(size_t num_entries)
+{
+  /* 
+   * This is inefficient, but allocate the table assuming one entry
+   * for each extension, so guaranteed to be big enough
+   */
+  F2M_Table* table = malloc(sizeof(F2M_Table));
+  if (!table) {
+    return NULL;
+  }
+  F2M_Entry* entry = calloc(num_entries, sizeof(F2M_Entry));
+  if (!entry) {
+    free(table);
+    return NULL;
+  }
+  table->max_size = num_entries;
+  table->size = 0;
+  table->entry = entry;
+  return table;
+}
+
+static int cmpfn(const void* l, const void* r)
+{
+  F2M_Entry* le = (F2M_Entry*) l;
+  F2M_Entry* re = (F2M_Entry*) r;
+  return (strcmp(le->key, re->key));
+}
+
+static F2M_Entry* find(const char* key, F2M_Table* table)
+{
+  F2M_Entry key_entry = { key, 0, 0 };
+  F2M_Entry* entry = (F2M_Entry*) bsearch(&key_entry, table->entry, table->size, sizeof(F2M_Entry), cmpfn);
+  return entry;
+}
+
+/*
+ * add: low perf implementation - should use hand-written bsearch if this is 
+ * important to find the spot to insert.
+ */
+static F2M_Entry* add(F2M_Entry* entry, F2M_Table* table)
+{
+  int i;
+  for (i=0; i<table->size; ++i) {
+    if (strcmp(table->entry[i].key, entry->key) > 0) {
+      printf("break at %d comparing %s to %s\n", i, entry->key, table->entry[i].key);
+      break;
+    }
+  }
+  printf("i after loop:%d\n", i);
+  memmove(&table->entry[i+1], &table->entry[i], (table->size-i)*sizeof(F2M_Entry));
+  memcpy(&table->entry[i], entry, sizeof(F2M_Entry));
+  table->size++;
+
+  for (i=0; i<table->size; ++i) {
+    printf("t[%d]: %s\n", i, table->entry[i].key);
+  }
+  return &table->entry[i];
+}
+
+static const char* extension(const char* path)
+{
+  char* dot = strrchr(path, '.');
+  if (dot) {
+    return dot+1;
+  } else {
+    return NULL;
+  }
+}
+
 #define MY_PATH_MAX (1024)
 int main(int argc, char* argv[])
 {
@@ -57,6 +139,7 @@ int main(int argc, char* argv[])
   char* dataset_pattern;
   int first_file_pattern;
   int rc;
+  F2M_Table* table;
 
   init_opts(&opts);
 
@@ -93,5 +176,26 @@ int main(int argc, char* argv[])
   }
 
   fprintf(stdout, "%d files to be processed\n", globset.gl_pathc);
+
+  /*
+   * Create an entry in a sorted table for each extension
+   * First pass is to get a count of the number of files with each
+   * extension
+   */
+
+  table = create(globset.gl_pathc);
+  for (i=0;i<globset.gl_pathc; ++i) {
+    const char* ext = extension(globset.gl_pathv[i]);
+    if (!ext || (ext[0] == '\0')) {
+      printf("%s has no extension - skipped\n", globset.gl_pathv[i]);
+      continue;
+    }
+    F2M_Entry* entry = find(ext, table);
+    if (!entry) {
+      F2M_Entry new_entry = { ext, 0, 0 };
+      add(&new_entry, table);
+    }
+  }
+  fprintf(stdout, "%d extensions to be processed\n", table->size);
   return 0;
 }
