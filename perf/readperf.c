9325168f2b11685b53890d67d2e10b49b6cba2cd
Mike Fulton <mikefultonpersonal@gmail.com>
2024-09-18 16:19:13 -0400

    Signed-off-by: Mike Fulton <mikefultonpersonal@gmail.com>
    Performance run of the 4 obvious ways to read and scan a file in.
    Reading by 'fd' (e.g. open/read/close) and one character at a time
    is the most efficient on z/OS (note on my Mac, this is not what I saw - buffering
    was more efficient).
    
    MFULTON@PLPSC:~/asmdio>time perf/readperf fd-buffer data/onehundredlines.txt
    
    real	0m0.662s
    user	0m0.075s
    sys	0m0.025s
    MFULTON@PLPSC:~/asmdio>time perf/readperf fd-buffer data/onehundredlines.txt
    
    real	0m0.715s
    user	0m0.075s
    sys	0m0.025s
    MFULTON@PLPSC:~/asmdio>time perf/readperf fp-buffer data/onehundredlines.txt
    
    real	0m1.087s
    user	0m0.109s
    sys	0m0.036s
    MFULTON@PLPSC:~/asmdio>time perf/readperf fp-buffer data/onehundredlines.txt
    
    real	0m1.114s
    user	0m0.109s
    sys	0m0.036s
    MFULTON@PLPSC:~/asmdio>time perf/readperf fd-char data/onehundredlines.txt
    
    real	0m0.634s
    user	0m0.058s
    sys	0m0.019s
    MFULTON@PLPSC:~/asmdio>time perf/readperf fd-char data/onehundredlines.txt
    
    real	0m0.518s
    user	0m0.053s
    sys	0m0.018s
    MFULTON@PLPSC:~/asmdio>time perf/readperf fp-char data/onehundredlines.txt
    
    real	0m0.931s
    user	0m0.117s
    sys	0m0.039s
    MFULTON@PLPSC:~/asmdio>time perf/readperf fp-char data/onehundredlines.txt
    
    real	0m0.989s
    user	0m0.114s
    sys	0m0.038s

--- perf/readperf.c
+++ perf/readperf.c
@@ -1,11 +1,13 @@
 #include <unistd.h>
-#include <strings.h>
+#include <string.h>
 #include <stdio.h>
 #include <fcntl.h>
 
 typedef enum {
-    BufferMethod=1,
-    CharMethod=2
+    FDBufferMethod=1,
+    FDCharMethod=2,
+    FPBufferMethod=3,
+    FPCharMethod=4
 } ReadMethod;
 
 #define ITERATIONS 1000
@@ -14,13 +16,67 @@ static int tot = 0;
 
 static void syntax(FILE* stream)
 {
-    fprintf(stream, "usage: readperf [char|buffer] file\n");
-    fprintf(stream, " where 'buffer' reads a buffer and scans chars\n");
-    fprintf(stream, " where 'char' reads a character at a time\n");
+    fprintf(stream, "usage: readperf [fd-char|fd-buffer|fp-char|fp-buffer] file\n");
+    fprintf(stream, " where 'fd-buffer' reads a buffer and scans chars\n");
+    fprintf(stream, " where 'fd-char' reads a character at a time\n");
+    fprintf(stream, " where 'fp-buffer' freads a buffer and scans chars\n");
+    fprintf(stream, " where 'fp-char' freads a character at a time\n");
     fprintf(stream, " and 'file' is the file to read\n");
 }
 
-int read_file_with_buffer(const char* filename)
+int fd_read_file_with_buffer(const char* filename)
+{
+    char buffer[32760];
+    int rc, i;
+    int fd = open(filename, O_RDONLY);
+
+    while (1) {
+        rc = read(fd, buffer, sizeof(buffer));
+        if (rc <= 0) {
+            break;
+        }
+        for (i=0; i < rc; ++i) {
+            tot += buffer[i];
+        }
+    }
+    close(fd);
+    return tot;
+}
+
+int fd_read_file_with_char(const char* filename)
+{
+    int rc, c, fd; 
+    fd = open(filename, O_RDONLY);
+    while (1) {
+       read(fd, &c, 1);
+       if (c < 0) {
+        break;
+       }
+       tot += c;
+    }  
+    close(fd);
+    return tot;
+}
+
+int fd_multi_read_with_buffer(const char* filename)
+{
+    int i;
+    for (i=0; i<ITERATIONS; ++i) {
+        tot += fd_read_file_with_buffer(filename);
+    }
+    return tot;
+}
+
+int fd_multi_read_with_char(const char* filename)
+{
+    int i;
+    for (i=0; i<ITERATIONS; ++i) {
+        tot += fd_read_file_with_char(filename);
+    }    
+    return tot;
+}
+
+int fp_read_file_with_buffer(const char* filename)
 {
     char buffer[32760];
     int rc, i;
@@ -38,7 +94,8 @@ int read_file_with_buffer(const char* filename)
     fclose(fp);
     return tot;
 }
-int read_file_with_char(const char* filename)
+
+int fp_read_file_with_char(const char* filename)
 {
     int rc, c; 
     FILE* fp = fopen(filename, "rb");
@@ -53,20 +110,20 @@ int read_file_with_char(const char* filename)
     return tot;
 }
 
-int multi_read_with_buffer(const char* filename)
+int fp_multi_read_with_buffer(const char* filename)
 {
     int i;
     for (i=0; i<ITERATIONS; ++i) {
-        tot += read_file_with_buffer(filename);
+        tot += fp_read_file_with_buffer(filename);
     }
     return tot;
 }
 
-int multi_read_with_char(const char* filename)
+int fp_multi_read_with_char(const char* filename)
 {
     int i;
     for (i=0; i<ITERATIONS; ++i) {
-        tot += read_file_with_char(filename);
+        tot += fp_read_file_with_char(filename);
     }    
     return tot;
 }
@@ -81,18 +138,24 @@ int main(int argc, char* argv[])
         syntax(stdout);
         return 0;
    }
-    if (!strcmp(method_name, "buffer")) {
-        method = BufferMethod;
-    } else if (!strcmp(method_name, "char")) {
-        method = CharMethod;
+    if (!strcmp(method_name, "fd-buffer")) {
+        method = FDBufferMethod;
+    } else if (!strcmp(method_name, "fd-char")) {
+        method = FDCharMethod;
+    } else if (!strcmp(method_name, "fp-buffer")) {
+        method = FPBufferMethod;
+    } else if (!strcmp(method_name, "fp-char")) {
+        method = FPCharMethod;
     } else {
         syntax(stderr);
         return 4;
     }
 
     switch(method) {
-        case BufferMethod: return multi_read_with_buffer(file_name);
-        case CharMethod: return multi_read_with_char(file_name);
+        case FDBufferMethod: return fd_multi_read_with_buffer(file_name);
+        case FDCharMethod: return fd_multi_read_with_char(file_name);
+        case FPBufferMethod: return fp_multi_read_with_buffer(file_name);
+        case FPCharMethod: return fp_multi_read_with_char(file_name);
     }
     printf("Character summation (to avoid premature optimization): %d\n", tot);
     return 4;
